45ed625c6c2764dd140e40a3f6e0be2f2e85dac4
lynni <lynni@DESKTOP-LK0PJF4>
2023-06-14 11:12:00 +0700

    Add project files.

---
+++ AutomobileSolution/Repository/CarRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutomobileSolution.BussinessObject;
+using AutomobileSolution.DataAccess;
+
+namespace AutomobileSolution.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public Car GetCarByID(int carID) => CarDBContext.Instance.GetCarByID(carID);
+        public IEnumerable<Car> GetCars() => CarDBContext.Instance.GetCarList;
+        public void InsertCar(Car car) => CarDBContext.Instance.AddNew(car);
+        public void DeleteCar(int carId) => CarDBContext.Instance.Remove(carId);
+        public void UpdateCar(Car car) => CarDBContext.Instance.Update(car);
+    }
+}
---
+++ AutomobileSolution/Repository/ICarRepository.cs
@@ -0,0 +1,15 @@
+﻿using AutomobileSolution.BussinessObject;
+using System.Collections;
+
+namespace AutomobileSolution.Repository
+{
+    public interface ICarRepository
+    {
+        public IEnumerable<Car> GetCars();
+        Car GetCarByID(int carId);
+        void InsertCar(Car car);
+        void DeleteCar(int carId);
+        void UpdateCar(Car car);
+    }
+}
+
